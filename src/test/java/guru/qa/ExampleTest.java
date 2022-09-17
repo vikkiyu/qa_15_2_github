660298af443eb05f50e5bd0599f28c46617b42d3
Victoria Yu <yuvictoria23@gmail.com>
2022-09-18 03:32:55 +0600

    another conflict

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -8,11 +8,11 @@ import java.util.Random;
 public class ExampleTest {
     @Test
     void exampleTest() {
-        Assertions.assertTrue(2 >= 1);
+        Assertions.assertTrue(2 >= 10);
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(3 >= 2);
+        Assertions.assertTrue(3 >= 20);
     }
 }
